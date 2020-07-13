fb89d49565cb27aeb3197d2a7969913bcadb02f0
minjj0905 <kmj2967@kookmin.ac.kr>
2020-07-13 16:14:00 +0900

    all move 2

---
+++ BOJ/2000/2600/2500/2560/2562.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int arr[9];
+    int n;
+    for(int i=0; i<9; i++) {
+        cin >> arr[i];
+    }
+    max(arr);
+}
