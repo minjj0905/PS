76d68572cbe5ed077a37afe183d9210b6cbb720e
minjj0905 <kmj2967@kookmin.ac.kr>
2020-07-15 16:56:15 +0900

    Update BOJ - 2164

---
+++ BOJ/2000/2100/2160/2164.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+int n;
+queue<int> q;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cin >> n;
+    for(int i=1; i<=n; i++) {
+        q.push(i);
+    }
+    while(q.size() > 1) {
+        q.pop();
+        q.push(q.front());
+        q.pop();
+    }
+    cout << q.front();
+}
