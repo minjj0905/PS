2b21013736b07ac3a33d937a8ace63b0b7ee061a
minjj0905 <minjj0905@kookmin.ac.kr>
2021-09-06 12:45:45 +0900

    11651 좌표 정렬하기 2

---
+++ BOJ/11000/11600/11650/11651.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    int n;
+    cin >> n;
+
+    int a, b;
+
+    vector<vector<int>> v;
+
+    while(n--) {
+      cin >> a >> b;
+      vector<int> xy;
+      xy.push_back(b);
+      xy.push_back(a);
+      v.push_back(xy);
+    }
+
+    sort(v.begin(), v.end());
+
+    for(int i=0; i<v.size(); i++) {
+      cout << v[i][1] << " " << v[i][0] << "\n";
+    }
+}
