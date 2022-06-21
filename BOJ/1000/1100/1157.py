d2a63b3b84e3cbcfab63512485acaf0c79e00a3b
minjj0905 <minjj0905@kookmin.ac.kr>
2022-06-21 16:53:25 +0900

    1157 단어공부

--- BOJ/1000/1100/1157.py
+++ BOJ/1000/1100/1157.py
@@ -1,3 +1,15 @@
 s = input()
-s.upper()
-print(s)
+s = s.upper()
+
+arr = {}
+for c in s:
+    if c in arr:
+        arr[c] += 1
+    else:
+        arr[c] = 1
+
+max_key = max(arr, key=arr.get)
+m = arr[max_key]
+del arr[max_key]
+
+print("?") if m in arr.values() else print(max_key)
