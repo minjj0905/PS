8651ed4f0a1ecf5d191a733ee5deaba8f3f0bd7a
minjj0905 <minjj0905@kookmin.ac.kr>
2022-10-06 13:26:30 +0900

    실전프로젝트 5주차

--- Algolab/실전프로젝트/5주차/계산기.py
+++ Algolab/실전프로젝트/5주차/계산기.py
@@ -1,25 +1,49 @@
 import sys
-from collections import deque
 
-def calc(q):
-  value = 0
-  signs = ["*", "+", "-"]
+def postfix(s):
+  prior = {'*':3,'/':3,'+':2,'-':2,'(':1} 
+  stack = []
+  ans = []
+  arr = s.split()
+  
+  for a in arr:
+    if a.isdigit():
+      ans.append(int(a))
+    elif a == "(":
+      stack.append(a)
+    elif a == ')':
+      while stack and stack[-1] != '(':
+        ans.append(stack.pop())
+      stack.pop()
+    else:
+      while stack and prior[a] <= prior[stack[-1]]:
+        ans.append(stack.pop())
+      stack.append(a)
 
-  for sign in signs:
-    cur = q.popleft()
-    
-  return value;
+  while stack:
+    ans.append(stack.pop())
+
+  return ans
+
+def calc(arr):
+  stack = []
+  
+  for a in arr:
+    if a == '+':
+      stack.append(stack.pop() + stack.pop())
+    elif a == '-':
+      stack.append(-(stack.pop() - stack.pop()))
+    elif a == '*':
+      stack.append(stack.pop() * stack.pop())
+    else:
+      stack.append(a)
+  
+  return stack.pop()
+  
 
 tc = int(sys.stdin.readline())
-
 for _ in range(tc):
-  l = int(sys.stdin.readline())
-  q = deque(sys.stdin.readline())
+  n = int(sys.stdin.readline())
+  s = sys.stdin.readline()
 
-  
-  arr = []
-
-  while(1):
-    cur = q.popleft()
-    # if cur == "(":
-      
+  sys.stdout.write(str(calc(postfix(s))) + '\n')
--- Algolab/실전프로젝트/5주차/주식투자.py
+++ Algolab/실전프로젝트/5주차/주식투자.py
@@ -1,25 +1,19 @@
 import sys
-from collections import deque
 
 tc = int(sys.stdin.readline())
 for _ in range(tc):
   d = int(sys.stdin.readline())
-  q = deque(list(map(int, sys.stdin.readline().split())))
-  cnt = 0
-
-  high = max(q)
-  if high == q[0]:
-    print(0)
-    continue
-
-  while(1):
-    cur = q.popleft()
-    cnt += high-cur
-    if len(q) == 0:
-      break
-    elif cur == high:
-      high = max(q)
-      if high == q[0]:
-        break
+  arr = list(map(int, sys.stdin.readline().split()))
   
-  print(cnt)
+  arr.reverse()
+  
+  max = arr[0]
+  profit = 0
+
+  for i in range(1, d):
+    if arr[i] > max:
+      max = arr[i]
+    else:
+      profit += max - arr[i]
+
+  sys.stdout.write(str(profit) + '\n')
