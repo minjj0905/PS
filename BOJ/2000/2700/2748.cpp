84f4080cb8f94c883cebc04a4d497530f9b0e507
minjj0905 <minjj0905@kookmin.ac.kr>
2022-10-26 05:04:13 +0900

    2748 피보나치 수 2

---
+++ BOJ/2000/2700/2748.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+#define ull unsigned long long
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    int n; cin >> n;
+    ull dp[91] = {0};
+    dp[0] = 0, dp[1] = 1;
+    for(int i=2; i<=n; i++) {
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+    cout << dp[n] << "\n";
+}
