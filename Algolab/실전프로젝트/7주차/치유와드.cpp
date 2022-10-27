5955c7bcb96edcc16b4c60eed3cb49412b4f06eb
minjj0905 <minjj0905@kookmin.ac.kr>
2022-10-27 14:49:32 +0900

    a

--- Algolab/실전프로젝트/7주차/치유와드.cpp
+++ Algolab/실전프로젝트/7주차/치유와드.cpp
@@ -1,73 +1,114 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+
+#define ll long long
+
 using namespace std;
+
+ll getDistance(pair <ll, ll> a, pair <ll, ll> b) {
+    return abs(a.first - b.first) + abs(a.second - b.second);
+}
+
+ll poisonEndT(pair <ll, ll> a, ll n) {
+    ll m = 0;
+    for(ll i=0; i<n; i+=n-1) {
+        m = max(m, llabs(i-a.first));
+        m = max(m, llabs(i-a.second));
+    }
+
+    return m;
+}
+
+ll healEndT(pair <ll, ll> a, ll n) {
+    ll m = 0;
+    for(ll i=0; i<n; i+=n-1) {
+        for(ll j=0; j<n; j+=n-1) {
+            m = max(m, getDistance(a, {i ,j}));
+        }
+    }
+
+    return m;
+}
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
     
-    int tc; cin >> tc;
+    ll tc; cin >> tc;
     while(tc--) {
-        int n, m; cin >> n >> m;
-        int a, b; cin >> a >> b;
+        ll n, m; cin >> n >> m;
+        ll a, b; cin >> a >> b;
 
-        vector<pair<int, int>> poison;
-        vector<pair<int, int>> heal;
+        vector<pair<ll, ll>> poison;
+        vector<pair<ll, ll>> heal;
 
-        int p_map[22][22] = {0, };
-        int h_map[22][22] = {0, };
-
-        pair<int, int> rc;
-        for(int i=0; i<a; i++) {
+        pair<ll, ll> rc;
+        for(ll i=0; i<a; i++) {
             cin >> rc.first >> rc.second;
             poison.push_back(rc);
         }
 
-        for(int i=0; i<b; i++) {
+        for(ll i=0; i<b; i++) {
              cin >> rc.first >> rc.second;
              heal.push_back(rc);
         }
 
-        for(int t=1; t<=m; t++) {
-            // 시체 와드 돌리기
-            for(int p=0; p<a; p++) {
-                for(int i=-t; i<=t; i++) {
-                    for(int j=-t; j<=t; j++) {
-                        if(poison[p].first + i >= 0 && poison[p].first + i < n && poison[p].second + j >= 0 && poison[p].second + j < n) {
-                            p_map[poison[p].first + i][poison[p].second + j]--;
+        ll map[21][21] = {0};
+
+        // 반복문 종료시점 구하기
+        ll maxT = 0;
+        for(ll i=0; i<a; i++) {
+            maxT = max(maxT, poisonEndT(poison[i], n));
+        }
+        for(ll i=0; i<b; i++) {
+            maxT = max(maxT, healEndT(heal[i], n));
+        }
+        
+        for(ll t=1; t<=min(maxT, m); t++) {
+            // 독
+            for(ll aa=0; aa<a; aa++){
+                for(ll i=-t; i<=t; i++) {
+                    for(ll j=-t; j<=t; j++) {
+                        ll x = poison[aa].first + i;
+                        ll y = poison[aa].second + j;
+                        if(x>=0 && x<n && y>=0 && y<n) {
+                            map[x][y]--;
                         }
                     }
                 }
             }
 
-            // 치유 와드 돌리기
-            for(int h=0; h<b; h++) {
-                for(int i=-t; i<=0; i++) {
-                    for(int j=-(i+t); j<=i+t; j++) {
-                        if(heal[h].first + i >= 0 && heal[h].first + i < n && heal[h].second + j >= 0 && heal[h].second + j < n) {
-                            h_map[heal[h].first + i][heal[h].second + j]++;
+            // 치유
+            for(ll bb=0; bb<b; bb++) {
+                for(ll i=-t; i<=t; i++) {
+                    if(i<=0) {
+                        for(ll j=-(i+t); j<=i+t; j++) {
+                            ll x = heal[bb].first + i;
+                            ll y = heal[bb].second + j;
+                            if(x>=0 && x<n && y>=0 && y<n) {
+                                map[x][y]++;
+                            }
                         }
-                    }
-                }
-                for(int i=1; i<=t; i++) {
-                    for(int j=i-t; j<=t-i; j++) {
-                        if(heal[h].first + i >= 0 && heal[h].first + i < n && heal[h].second + j >= 0 && heal[h].second + j < n) {
-                            h_map[heal[h].first + i][heal[h].second + j]++;
+                    } else {
+                        for(ll j=i-t; j<=t-i; j++) {
+                            ll x = heal[bb].first + i;
+                            ll y = heal[bb].second + j;
+                            if(x>=0 && x<n && y>=0 && y<n) {
+                                map[x][y]++;
+                            }
                         }
                     }
                 }
             }
-            
         }
 
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<n; j++) {
-                cout << p_map[i][j]+h_map[i][j] << ' ';
+        for(ll i=0; i<n; i++) {
+            for(ll j=0; j<n; j++) {
+                cout << map[i][j] << ' ';
             }
             cout << "\n";
         }
-
     }
 }
