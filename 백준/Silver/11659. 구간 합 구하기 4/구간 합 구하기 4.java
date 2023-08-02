4f391555037f0c395b8651cfd88a440525c8f102
KIM MIN JEONG <minjj0905@daum.net>
2023-08-02 14:33:54 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 2052 ms, Memory: 63572 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    int n = Integer.parseInt(st.nextToken());
+    int m = Integer.parseInt(st.nextToken());
+
+    int[] arr = new int[n];
+    st = new StringTokenizer(br.readLine());
+
+    for (int i = 0; i < n; i++) {
+      arr[i] = Integer.parseInt(st.nextToken());
+    }
+
+    int[] prefix = new int[n];
+    prefix[0] = arr[0];
+
+    for (int i = 1; i < n; i++) {
+      prefix[i] = prefix[i - 1] + arr[i];
+    }
+
+    for (int i = 0; i < m; i++) {
+      st = new StringTokenizer(br.readLine());
+      int a = Integer.parseInt(st.nextToken());
+      int b = Integer.parseInt(st.nextToken());
+      System.out.println(prefix[b - 1] - prefix[a - 1] + arr[a - 1]);
+    }
+  }
+}
