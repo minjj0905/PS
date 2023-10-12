cf4ac9f8d2c61b3e13a44ef0976fd2a26c404abf
KIM MIN JEONG <minjj0905@daum.net>
2023-10-12 11:55:43 +0900

    [Silver V] Title: 피보나치 수 4, Time: 272 ms, Memory: 24928 KB -BaekjoonHub

---
+++ 백준/Silver/10826. 피보나치 수 4/피보나치 수 4.java
@@ -0,0 +1,28 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        BigInteger result = fibonacci(n);
+        System.out.println(result);
+    }
+
+    static BigInteger fibonacci(int n) {
+        if (n <= 1) {
+            return BigInteger.valueOf(n);
+        }
+
+        BigInteger[] fib = new BigInteger[n + 1];
+        fib[0] = BigInteger.ZERO;
+        fib[1] = BigInteger.ONE;
+
+        for (int i = 2; i <= n; i++) {
+            fib[i] = fib[i - 1].add(fib[i - 2]);
+        }
+
+        return fib[n];
+    }
+}
