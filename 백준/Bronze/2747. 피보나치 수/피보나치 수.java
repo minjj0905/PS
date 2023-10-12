7d0a2a9ab5388e56eaf3c4ae17c1c56be27c1c2c
KIM MIN JEONG <minjj0905@daum.net>
2023-10-12 11:27:34 +0900

    [Bronze II] Title: 피보나치 수, Time: 204 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        int[] dp = new int[46];
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        System.out.println(dp[n]);
+    }
+}
