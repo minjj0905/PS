55073a11cd1ecbe8ed91434e72c5136966953c2d
KIM MIN JEONG <minjj0905@daum.net>
2024-05-11 16:32:07 +0900

    [level 2] Title: 전화번호 목록, Time: 101.42 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -0,0 +1,10 @@
+function solution(phone_book) {
+    const dict = [];
+    
+    phone_book.sort();
+    
+    for (let i=1; i<phone_book.length; i++) {
+        if(phone_book[i].startsWith(phone_book[i-1])) return false;
+    }
+    return true;
+}
